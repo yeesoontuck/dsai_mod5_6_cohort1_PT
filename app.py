4c3dcefa90a7f7b29eada9ab11477dd844821cdb
Yee Soon Tuck <yeesoontuck@gmail.com>
2025-05-13 13:15:35 +0000

    gemini route and view

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/gemini', methods=["POST"])
+def gemini():
+    return render_template("gemini.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
