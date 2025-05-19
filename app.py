fbc4b8ddef1719807c097e3e918c6563d45c65a6
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-05-19 17:17:00 +0800

    add Telegram webhook handler

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, request, render_template
 import google.generativeai as genai
 from markdown2 import Markdown
 from dotenv import load_dotenv
+load_dotenv()
 import os
 
 from google import genai as genai_new
 from google.genai import types
+
 from PIL import Image
 from io import BytesIO
-# import random
 import time
+import requests
+from datetime import datetime
 
-load_dotenv()
-gemini_api_key = os.getenv('GEMINI_API_KEY')
+# Gemini Telegram chat
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
+client = genai_new.Client(api_key=GEMINI_API_KEY)
+url = f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/'
 
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
 
@@ -41,7 +45,7 @@ def gemini_reply():
 
 
 
-@app.route('/experimental', methods=['GET'])
+@app.route('/experimental/gemini_text', methods=['GET'])
 def experimental():
     return render_template('experimental/gemini_text.html')
 
@@ -91,5 +95,42 @@ def gemini_image_output():
 
     return render_template('experimental/gemini_image.html', prompt=contents, image=filename)
 
+
+
+@app.route('/experimental/telegram/webhook', methods=['POST'])
+def telegram_webhook():
+    data = request.get_json()  # Parse JSON payload
+    message = data.get('message')
+    
+    chat_id = message['chat']['id']
+    message_text = message.get('text', '')
+
+    if message_text.lower() == '/start':
+        requests.get(url + f'sendMessage?chat_id={chat_id}&text={"Ask me a question: (or type quit)"}')
+        return 'OK', 200
+    elif message_text.lower() == "/quit":
+        requests.get(url + f'sendMessage?chat_id={chat_id}&text=Good%20Bye!')
+        return 'OK', 200
+    else:
+        # trigger Gemini API
+        gemini_result = client.models.generate_content(
+            model="gemini-2.0-flash",
+            config=types.GenerateContentConfig(
+                system_instruction="Use fewer than 101 words in all replies.."),
+            contents=[message_text]
+        )
+
+        # show "typing..."
+        send_action = url + f'sendChatAction?chat_id={chat_id}&action=typing'
+        requests.get(send_action)
+
+        r = gemini_result.text
+        send_url = url + f'sendMessage?parse_mode=markdown&chat_id={chat_id}&text={r}'
+        requests.get(send_url)
+
+        return 'OK', 200
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
+    # app.run()
